d8ea5844913b70371be3b4a581cb346e81ff5067
ckhaziq <92288630+ckhaziq@users.noreply.github.com>
2023-02-12 16:12:09 +0800

    2

--- readForm.php
+++ readForm.php
@@ -18,7 +18,10 @@ else{
     $title =  "No Title";
 }
 
-$con = mysqli_connect("ca19009-lab11-server.mysql.database.azure.com","dfyhiqizfj","4M31IQQPH470C85J$","ca19009-lab11-database") or die(mysqli_connect_error());
+//$con = mysqli_connect("ca19009-lab11-server.mysql.database.azure.com","dfyhiqizfj","4M31IQQPH470C85J$","ca19009-lab11-database") or die(mysqli_connect_error());
+$con = mysqli_init();
+mysqli_ssl_set($con,NULL,NULL, "{path to CA cert}", NULL, NULL);
+mysqli_real_connect($conn, "ca19009-lab11-server.mysql.database.azure.com", "dfyhiqizfj", "4M31IQQPH470C85J$", "ca19009-lab11-database", 3306, MYSQLI_CLIENT_SSL);
 
 // to select the targeted database
 mysqli_select_db($con,"mydb") or die(mysqli_error());
